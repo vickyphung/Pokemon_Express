de97e81ae5d7d86a2c0eee606adf4f6456ebc98f
Vicky Phung <vickyphung@gmail.com>
2022-04-29 13:43:49 -0400

    Server set up and running.

---
+++ server.js
@@ -0,0 +1,29 @@
+//imports
+const express = require("express")
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+const cors = require("cors")
+
+
+//configure
+require("dotenv").config()
+
+const server = express()
+const PORT = process.env.PORT || 3000
+
+// middleware
+server.use(cors("*"))
+server.use(morgan("dev"))
+server.use(express.json())
+server.use(bodyParser.json())
+
+// // ROUTES
+// server.use("/auth", authRouter)
+
+server.get("/", (req, res)=>{
+    res.status(200).json({message: 'Welcome to the Pokemon App!'})
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server is listening at ${PORT}`)
+})
