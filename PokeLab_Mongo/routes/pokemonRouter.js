2581c1529cda5a0998fb2bd7ddbdc181eaec3090
Vicky Phung <vickyphung@gmail.com>
2022-05-02 13:38:07 -0400

    updated pokemonRouter.js in PokeLab_Mongo

--- PokeLab_Mongo/routes/pokemonRouter.js
+++ PokeLab_Mongo/routes/pokemonRouter.js
@@ -1,17 +1,10 @@
 const express = require("express")
-//IMPORT MODEL
 const pokemon = require("../schema/pokeSchema")
 
 const pokeRoute = express.Router()
 
 pokeRoute.get("/", (req, res)=>{
-    //find CAN take a query, and will return ALL THINGS in requested table
-    // that matches the query
-    // HOMIES OF MONGOOSE
-
-    // READ
     pokemon.find((err, allPokemon)=>{
-        //ERRORS GO FIRST
         if(err){
             res.status(404).json({message: err.message})
         } else {
@@ -20,10 +13,8 @@ pokeRoute.get("/", (req, res)=>{
     })
 })
 
-//POST - STILL TAKES BODY
 pokeRoute.post("/", (req, res)=>{
     const newPokemon = req.body
-
     pokemon.create(newPokemon, (err, pokemon)=>{
         if(err){
             res.status(400).json({message: err.message})
@@ -33,44 +24,6 @@ pokeRoute.post("/", (req, res)=>{
     })
 })
 
-// pokeRoute.get("/:name", (req, res)=>{
-//     //still comes in from params
-//     const name = req.params.name
-//     pokemon.find(name, (err, pokemon)=>{
-//         if(err){
-//             res.status(404).json({message: err.message})
-//         }else{
-//             res.status(200).json(pokemon)
-//         }
-//     })
-// })
-
-
-
-// pokeRoute.put("/:id", (req, res)=>{
-//     const id = req.params.id
-//     const updatedPokemon = req.body
-
-//     pokemon.findByIdAndUpdate(id, updatedPokemon, {new: true},(err, updatedPokemon)=>{
-//         if(err){
-//             res.status(404).json({message: err.message})
-//         } else {
-//             res.status(202).json(updatedPokemon)
-//         }
-//     })
-// })
-
-// pokeRoute.delete("/:id",(req, res)=>{
-//     const id = req.params.id
-
-//     pokemon.findByIdAndDelete(id, (err)=>{
-//         if(err){
-//             res.status(404).json({message: err.message})
-//         }else{
-//             res.status(204).json({message: "DELETED"})
-//         }
-//     })
-// })
 
 pokeRoute.get("/:name", (req, res)=>{
     const name = req.params.name
